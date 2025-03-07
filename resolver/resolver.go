31235c44715b075cfbaf5aa9f2c0cc996d8d9228
iamrosada0 <luisrosada@outlook.com>
2025-03-07 16:59:53 +0100

    feat: the code is working well

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -1,29 +1,8 @@
 package resolver
 
-// THIS CODE WILL BE UPDATED WITH SCHEMA CHANGES. PREVIOUS IMPLEMENTATION FOR SCHEMA CHANGES WILL BE KEPT IN THE COMMENT SECTION. IMPLEMENTATION FOR UNCHANGED SCHEMA WILL BE KEPT.
-
-import (
-	"context"
-	"gqlgen_test/generated"
-	"gqlgen_test/model"
-)
+import "gqlgen_test/generated"
 
 type Resolver struct{}
-
-// CreatePost is the resolver for the createPost field.
-func (r *mutationResolver) CreatePost(ctx context.Context, title string, content string) (*model.Post, error) {
-	panic("not implemented")
-}
-
-// Users is the resolver for the users field.
-func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
-	panic("not implemented")
-}
-
-// Posts is the resolver for the posts field.
-func (r *queryResolver) Posts(ctx context.Context) ([]*model.Post, error) {
-	panic("not implemented")
-}
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
